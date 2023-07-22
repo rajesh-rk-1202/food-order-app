82d092937951c51dae18caa0f6a625ef2f9086dd
Rajesh Kumar Jena <jenarajeshkumar768@gmail.com>
2023-07-22 13:06:58 +0530

    added mealItemForm component to add the mealItem to the cart

---
+++ src/components/UI/Input.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import classes from "./Input.module.css";
+
+const Input = (props) => {
+  console.log("IDs", props.input.id);
+  return (
+    <div className={classes.input}>
+      <label htmlFor={props.input.id}>{props.label}</label>
+      <input {...props.input}></input>
+    </div>
+  );
+};
+
+export default Input;
