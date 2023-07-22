694c4ccfbea554b5e1417a10be1e3e663b0a9b36
Rajesh Kumar Jena <jenarajeshkumar768@gmail.com>
2023-07-22 16:10:12 +0530

    added cart Modal and changed some styling as well

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React from "react";
+import { Fragment } from "react";
 
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 
 function App() {
   return (
-    <React.Fragment>
-      <Header></Header>
+    <Fragment>
+      <Cart />
+      <Header />
       <main>
-        <Meals></Meals>
+        <Meals />
       </main>
-    </React.Fragment>
+    </Fragment>
   );
 }
 
